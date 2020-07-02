715aad5def46c98897810e5c730e196c455bb637
anujiuee <anujkunalarora@gmail.com>
2020-07-02 23:05:09 +0530

    1 ms

---
+++ solutions/1. Two Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int n = nums.length;
+        
+        HashMap<Integer,Integer> m = new HashMap();
+        
+        for (int i=0;i<n;i++){
+            
+            if(m.containsKey(target-nums[i])){
+                return new int []{m.get(target-nums[i]),i};
+            }
+            m.put(nums[i],i);
+        }
+        
+        return new int []{-1,-1};
+    }
+}
