8545b20261fb9872e9ec111ad5a22619acfb0de1
anujiuee <anujkunalarora@gmail.com>
2020-07-03 01:39:47 +0530

    7 ms

--- solutions/253. Meeting Rooms II.java
+++ solutions/253. Meeting Rooms II.java
@@ -37,3 +37,22 @@ class Solution {
    return max;     
     }
 }
+
+/*
+
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if(intervals.length <= 1) 
+            return intervals.length;
+        Arrays.sort(intervals, (i1,i2)->i1[0] - i2[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((i1,i2)->i1[1] - i2[1]);
+        pq.add(intervals[0]);
+        for(int i=1;i<intervals.length;i++) {
+            if(intervals[i][0] >= pq.peek()[1]) 
+                pq.remove();
+            pq.add(intervals[i]);
+        }
+        return pq.size();
+    }
+}
+*/
