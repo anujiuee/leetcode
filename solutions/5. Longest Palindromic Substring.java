fc5c6439fdf17bfbe43f5a3e462296e7cb638f8e
anujiuee <anujkunalarora@gmail.com>
2020-07-16 19:25:36 +0530

    111 ms

--- solutions/5. Longest Palindromic Substring.java
+++ solutions/5. Longest Palindromic Substring.java
@@ -18,47 +18,5 @@ class Solution {
   }
     
   return res;
-        
     }
-   
-    
-      /*
-
- int max=Integer.MIN_VALUE;
-    String result="";
-    
-    private int checkForPalindrome(String s, int start, int end){
-        if(s.charAt(start) != s.charAt(end)) return 0;
-        while(start>=0 && end <s.length() && s.charAt(start) == s.charAt(end)){
-            start--;
-            end++;
-        }
-        return end-start-1;
-    }
-    
-    public String longestPalindrome(String s) {
-        if(s.length()<2) return s;
-        for(int i=0;i<s.length()-1;i++){
-            int l1 = checkForPalindrome(s,i,i);
-            int l2 = checkForPalindrome(s,i,i+1);
-            int most = Math.max(l1,l2);
-            if(most>max){
-                max=most;
-                if(most%2==0){
-                    result = s.substring(i-((most/2)-1),i+(most/2)+1);
-                }else{
-                    result = s.substring(i-((most/2)),i+(most/2)+1);
-                }
-            }
-        }
-        return result;
-    }
-
-
-
-
-*/
-  
 }
-
-
