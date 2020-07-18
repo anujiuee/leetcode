a643369f70bc4381db28e750360494ff601fbb86
anujiuee <anujkunalarora@gmail.com>
2020-07-18 22:28:04 +0530

    Update 1254. Number of Closed Islands.java

--- solutions/1254. Number of Closed Islands.java
+++ solutions/1254. Number of Closed Islands.java
@@ -30,3 +30,45 @@ class Solution {
 		return ans;
 	}
 }
+
+/*
+
+
+class Solution {
+    public int closedIsland(int[][] grid) {
+        int count =0;
+        
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j< grid[0].length; j++){
+                
+                if(grid[i][j] == 0 && dfs(i, j, grid))
+                    count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    public boolean dfs(int i, int j, int[][] grid){
+        
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length)
+            return false;
+        
+        if((i==0 || i==grid.length || j==0 || j==grid[0].length) && grid[i][j] == 0)
+            return false;
+        
+        if(grid[i][j] == 0)
+            grid[i][j] = 1;
+        else
+            return true;
+        
+        boolean top = dfs(i-1, j, grid);
+        boolean down = dfs(i+1, j, grid);
+        boolean left = dfs(i, j-1, grid);
+        boolean right = dfs(i, j+1, grid);
+        
+        return (top && down && left && right);
+    }
+}
+
+*/
