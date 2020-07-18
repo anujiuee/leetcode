02102db38f2e12642357a427fe35aa7a7be33058
anujiuee <anujkunalarora@gmail.com>
2020-07-18 20:11:44 +0530

    0 ms

---
+++ solutions/1306. Jump Game III.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+        boolean [] isVisited = new boolean[n];
+        Queue<Integer> q = new LinkedList();
+        q.add(start);
+        isVisited[start]=true;
+        
+        while(!q.isEmpty()){
+            int k = q.peek();
+            if(arr[k]==0) return true;
+             isVisited[k]=true;
+            q.remove();
+            if(k+arr[k]<n && !isVisited[k+arr[k]]){
+               q.add(k+arr[k]); 
+            }
+            
+             if(k-arr[k]>-1 && !isVisited[k-arr[k]]){
+               q.add(k-arr[k]); 
+            }
+        }
+        
+        
+        return false;
+        
+    }
+}
