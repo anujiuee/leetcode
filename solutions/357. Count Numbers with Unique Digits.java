b5cb180c77e4a506959f5ea333ee2a53c29fa5b9
anujiuee <anujkunalarora@gmail.com>
2020-07-18 16:36:19 +0530

    0 ms

---
+++ solutions/357. Count Numbers with Unique Digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+      if (n == 0) {
+        return 1;
+    }
+    int ans = 10, base = 9;
+    for (int i = 2; i <= n && i <= 10; i++) {
+        base = base * (9 - i + 2);
+        ans += base;
+    }
+    return ans;
+    }
+}
