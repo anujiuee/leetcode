751d326d2e356d3eb29cc66f197cd966b58b8fda
anujiuee <anujkunalarora@gmail.com>
2020-07-23 20:44:49 +0530

    0 ms

---
+++ solutions/48. Rotate Image.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int n = matrix.length;
+        
+        int a,b,c,d;
+        
+        for(int i=0; i<(n+1)/2; i++){
+            for(int j=i; j<n-i-1; j++){
+                 a = matrix[i][j];
+                 b = matrix[j][n-i-1];
+                 c = matrix[n-i-1][n-j-1];
+                 d = matrix[n-j-1][i];
+                matrix[j][n-i-1] = a;
+                matrix[n-i-1][n-j-1] = b;
+                matrix[n-j-1][i] = c;
+                matrix[i][j] = d;
+            }
+        }
+    }
+}
