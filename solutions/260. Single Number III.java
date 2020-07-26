e1213b0f5c660942c9b759d24b869b273a4fcbbf
anujiuee <anujkunalarora@gmail.com>
2020-07-26 12:35:07 +0530

    1 ms

--- solutions/260. Single Number III.java
+++ solutions/260. Single Number III.java
@@ -1,29 +1,26 @@
 class Solution {
-    public int[] singleNumber(int[] nums) {
-        
-        int a = nums[0];
-        
-        for (int i=1;i<nums.length;i++){
-            a= nums[i]^a;
-        }
-        
-        int b = a & (a-1);
-        b = b^a;
-        
-        int m =0;
-        int n =0;
-        
-        for (int i=0;i<nums.length;i++){
-            if((b & nums[i]) !=0){
-                m=m^nums[i];
-            }else {
-                n=n^nums[i];
-            }
-        }
-        
-        
-        
-        return new int []{m,n};
-        
-    }
+    public int[] singleNumber(int[] nums) {
+        
+        int a = nums[0];
+        
+        for (int i=1;i<nums.length;i++){
+            a= nums[i]^a;
+        }
+        
+        int b = a & (a-1);
+        b = b^a;
+        
+        int m =0;
+        
+        for (int i=0;i<nums.length;i++){
+            if((b & nums[i]) !=0){
+                m=m^nums[i];
+            }
+        }
+        
+        
+        
+        return new int []{m,m^a};
+        
+    }
 }
