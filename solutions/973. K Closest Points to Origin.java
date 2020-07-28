a80218661702a96402ecc80df8fc9148ca7320f7
anujiuee <anujkunalarora@gmail.com>
2020-07-28 18:33:11 +0530

    22 ms

---
+++ solutions/973. K Closest Points to Origin.java
@@ -0,0 +1,48 @@
+        this.points = points;
+        sort(0, points.length - 1, K);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+​
+    public void sort(int i, int j, int K) {
+        if (i >= j) return;
+        int k = ThreadLocalRandom.current().nextInt(i, j);
+        swap(i, k);
+​
+        int mid = partition(i, j);
+        int leftLength = mid - i + 1;
+        if (K < leftLength)
+            sort(i, mid - 1, K);
+        else if (K > leftLength)
+            sort(mid + 1, j, K - leftLength);
+    }
+​
+    public int partition(int i, int j) {
+        int oi = i;
+        int pivot = dist(i);
+        i++;
+​
+        while (true) {
+            while (i < j && dist(i) < pivot)
+                i++;
+            while (i <= j && dist(j) > pivot)
+                j--;
+            if (i >= j) break;
+            swap(i, j);
+        }
+        swap(oi, j);
+        return j;
+    }
+    
+     public int dist(int i) {
+        return points[i][0] * points[i][0] + points[i][1] * points[i][1];
+    }
+​
+    public void swap(int i, int j) {
+        int t0 = points[i][0], t1 = points[i][1];
+        points[i][0] = points[j][0];
+        points[i][1] = points[j][1];
+        points[j][0] = t0;
+        points[j][1] = t1;
+    }
+​
+}
