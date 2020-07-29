d89e000b9b168b73ec237fa5adee9768afb152bd
anujiuee <anujkunalarora@gmail.com>
2020-07-29 20:17:54 +0530

    N/A

--- solutions/134. Gas Station.java
+++ solutions/134. Gas Station.java
@@ -1,15 +1,22 @@
 class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-       int  totalGas=0,index=0,require=0;
-        for(int i=0;i<gas.length;i++){
-           totalGas+=gas[i]-cost[i];   
-            if(totalGas<0){
-                require+=cost[i]-Math.abs(totalGas);
-                index=i+1;
-                totalGas=0;             
-            }
-        }
-        
-        return totalGas>=require?index:-1;
-    }
-}
+  public:
+  int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+    int n = gas.size();
+​
+    int total_tank = 0;
+    int curr_tank = 0;
+    int starting_station = 0;
+    for (int i = 0; i < n; ++i) {
+      total_tank += gas[i] - cost[i];
+      curr_tank += gas[i] - cost[i];
+      // If one couldn't get here,
+      if (curr_tank < 0) {
+        // Pick up the next station as the starting one.
+        starting_station = i + 1;
+        // Start with an empty tank.
+        curr_tank = 0;
+      }
+    }
+    return total_tank >= 0 ? starting_station : -1;
+  }
+};
