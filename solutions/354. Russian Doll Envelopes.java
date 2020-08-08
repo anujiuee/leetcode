5c6bc09a2443c9065ce9cc40e9ec2469167f1b89
anujiuee <anujkunalarora@gmail.com>
2020-08-08 18:22:13 +0530

    272 ms

--- solutions/354. Russian Doll Envelopes.java
+++ solutions/354. Russian Doll Envelopes.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int maxEnvelopes(int[][] envelopes) {
-        
-        Arrays.sort(envelopes,(i,j)->(i[0]*i[1]-j[0]*j[1]));
-        
-        int n = envelopes.length;
-        
-        int ans[]= new int [n];
-        
-        int k =0;
-        
-        int t =0;
-        
-        for (int i=0;i<n;i++){
-            ans[i]=1;k=0;
-            for (int j=0;j<i;j++){
-                if(envelopes[i][0]>envelopes[j][0] && envelopes[i][1]>envelopes[j][1]){
-                  k= Math.max(ans[j],k);
-                }
-            }
-            ans[i]=Math.max(ans[i],k+1);
-            t=Math.max(ans[i],t);
-        }
-        
-        return t;
-    }
+    public int maxEnvelopes(int[][] envelopes) {
+        
+        Arrays.sort(envelopes,(i,j)->(i[0]*i[1]-j[0]*j[1]));
+        
+        int n = envelopes.length;
+        
+        int ans[]= new int [n];
+        
+        int k =0;
+        
+        int t =0;
+        
+        for (int i=0;i<n;i++){
+            ans[i]=1;k=0;
+            for (int j=0;j<i;j++){
+                if(envelopes[i][0]>envelopes[j][0] && envelopes[i][1]>envelopes[j][1]){
+                  k= Math.max(ans[j],k);
+                }
+            }
+            ans[i]=Math.max(ans[i],k+1);
+            t=Math.max(ans[i],t);
+        }
+        
+        return t;
+    }
 }
