a19b2dddec8b1450495690793f14a4bdda68064c
anujiuee <anujkunalarora@gmail.com>
2020-08-08 20:01:45 +0530

    1 ms

---
+++ solutions/103. Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+if (root == null) return res;
+Queue<TreeNode> queue = new LinkedList<>();
+queue.add(root);
+boolean zigzag = false;
+while (!queue.isEmpty()) {
+    List<Integer> level = new ArrayList<>();
+    int cnt = queue.size();
+    for (int i = 0; i < cnt; i++) {
+        TreeNode node = queue.poll();
+        if (zigzag) {
+            level.add(0, node.val);
+        }
+        else {
+            level.add(node.val);
+        }
+        if (node.left != null) {
+            queue.add(node.left);
+        }
+        if (node.right != null) {
+            queue.add(node.right);
+        }
+    }
+    res.add(level);
+    zigzag = !zigzag;
+}
+return res;
+    }
+    
+    
+    /*
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
+    {
+        List<List<Integer>> sol = new ArrayList<>();
+        travel(root, sol, 0);
+        return sol;
+    }
+    
+    private void travel(TreeNode curr, List<List<Integer>> sol, int level)
+    {
+        if(curr == null) return;
+        
+        if(sol.size() <= level)
+        {
+            List<Integer> newLevel = new LinkedList<>();
+            sol.add(newLevel);
+        }
+        
+        List<Integer> collection  = sol.get(level);
+        if(level % 2 == 0) collection.add(curr.val);
+        else collection.add(0, curr.val);
+        
+        travel(curr.left, sol, level + 1);
+        travel(curr.right, sol, level + 1);
+    }
+    */
+    
+}
