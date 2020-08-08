644026697c8d6988da10c14633d45847546e7646
anujiuee <anujkunalarora@gmail.com>
2020-08-08 14:02:12 +0530

    0 ms

---
+++ solutions/270. Closest Binary Search Tree Value.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public int closestValue(TreeNode root, double target) {
+    int val, closest = root.val;
+    while (root != null) {
+      val = root.val;
+      closest = Math.abs(val - target) < Math.abs(closest - target) ? val : closest;
+      root =  target < root.val ? root.left : root.right;
+    }
+    return closest;
+  }
+}
