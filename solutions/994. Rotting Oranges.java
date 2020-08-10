0ff3efa898062940771833ea31fc1a842e870c40
anujiuee <anujkunalarora@gmail.com>
2020-08-10 12:16:52 +0530

    Update 994. Rotting Oranges.java

--- solutions/994. Rotting Oranges.java
+++ solutions/994. Rotting Oranges.java
@@ -1,47 +1,83 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        
-        int n = grid.length;
-        
-        int m = grid[0].length;
-        
-        if(n==0 && m ==0) return 0;
-        
-        Queue<int []> q= new LinkedList();
-        
-        int count =0;
-        
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if(grid[i][j]==2) {q.add(new int []{i,j});}
-                if(grid[i][j]==1) count++;
-            }  
-        }
-        
-        
-        int time = -1;
-        
-        while(!q.isEmpty()){
-            
-            int size = q.size();
-            
-            time++;
-            
-          //   boolean exist = false;
-​
-            
-            for(int t=0;t<size;t++){
-                
-                int [] k = q.poll();
-                int i = k[0];
-                int j = k[1];
-                                
-                
-                if(i+1<n && grid[i+1][j]==1){
-                    q.add(new int []{i+1,j});
-                    grid[i+1][j]=2;
-                    count--;
-                   // exist = true;
-                }
-                
-                if(j+1<m && grid[i][j+1]==1){
+    public int orangesRotting(int[][] grid) {
+        
+        int n = grid.length;
+        
+        int m = grid[0].length;
+        
+        if(n==0 && m ==0) return 0;
+        
+        Queue<int []> q= new LinkedList();
+        
+        int count =0;
+        
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if(grid[i][j]==2) {q.add(new int []{i,j});}
+                if(grid[i][j]==1) count++;
+            }  
+        }
+        
+        
+        int time = -1;
+        
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            
+            time++;
+            
+          //   boolean exist = false;
+
+            
+            for(int t=0;t<size;t++){
+                
+                int [] k = q.poll();
+                int i = k[0];
+                int j = k[1];
+                                
+                
+                if(i+1<n && grid[i+1][j]==1){
+                    q.add(new int []{i+1,j});
+                    grid[i+1][j]=2;
+                    count--;
+                   // exist = true;
+                }
+                
+                if(j+1<m && grid[i][j+1]==1){
+                    q.add(new int []{i,j+1});
+                    grid[i][j+1]=2;
+                    count--;
+                    //exist = true;
+                }
+                
+                if(i-1>=0 && grid[i-1][j]==1){
+                    q.add(new int []{i-1,j});
+                    grid[i-1][j]=2;
+                    count--;
+                  //   exist = true;
+                }
+                
+                if(j-1>=0 && grid[i][j-1]==1){
+                    q.add(new int []{i,j-1});
+                    grid[i][j-1]=2;
+                    count--;
+                    // exist = true;
+                }
+                
+                
+            }
+            
+             //if(exist) time++;
+            
+            
+        }
+        
+        
+        if(time==-1 && count==0) return 0;
+        if(count==0) return time-1;
+        
+        return -1;
+        
+    }
+}
