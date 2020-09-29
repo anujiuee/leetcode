907d216087f79551f07318ca49220ef1c8d8097e
anujiuee <anujkunalarora@gmail.com>
2020-09-29 20:48:30 +0530

    2 ms

--- solutions/545. Boundary of Binary Tree.java
+++ solutions/545. Boundary of Binary Tree.java
@@ -1,3 +1,39 @@
+​
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+​
+    public boolean isLeaf(TreeNode t) {
+        return t.left == null && t.right == null;
+    }
+​
+    public void addLeaves(List<Integer> res, TreeNode root) {
+        if (isLeaf(root)) {
+            res.add(root.val);
+        } else {
+            if (root.left != null) {
+                addLeaves(res, root.left);
+            }
+            if (root.right != null) {
+                addLeaves(res, root.right);
+            }
+        }
+    }
+​
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        if (!isLeaf(root)) {
+            res.add(root.val);
         }
         TreeNode t = root.left;
         while (t != null) {
