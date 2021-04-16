896d3750c7505578e368e84778cf0d69e3610b38
anujiuee <anujkunalarora@gmail.com>
2021-04-16 23:25:44 +0530

    0 ms

--- solutions/98. Validate Binary Search Tree.java
+++ solutions/98. Validate Binary Search Tree.java
@@ -13,38 +13,24 @@
  *     }
  * }
  */
-​
-/*
 class Solution {
     public boolean isValidBST(TreeNode root) {
         
-       return helper(root, null, null);
-    }
-    
-    boolean helper(TreeNode node, Integer left, Integer right){
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
         
-        if(node == null)
-            return true;
-        
-        if(left != null && node.val <= left)
-            return false;
-        else if(right != null && node.val >= right)
-            return false;
-        
-        return helper(node.left, left, node.val) && helper(node.right, node.val, right);
-    }
-}
-*/
-​
-​
-public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
-    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
-        if (root == null) return true;
-        if (root.val >= maxVal || root.val <= minVal) return false;
-        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
-    }
+    
+     public boolean isValidBST(TreeNode root,long min, long max) {
+        
+         if(root==null) return true;
+         int val = root.val;
+         
+         if(val<=min || val>=max) return false;
+        
+         return isValidBST(root.left,min,val) && isValidBST(root.right,val,max);
+        
+      }
+    
+    
 }
