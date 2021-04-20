65375e36581b82207375ebbb7d87bdc733190942
anujiuee <anujkunalarora@gmail.com>
2021-04-20 20:25:56 +0530

    3 ms

--- solutions/518. Coin Change 2.java
+++ solutions/518. Coin Change 2.java
@@ -1,20 +1,14 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-        int[] temp=new int[amount+1];
-        temp[0]=1;
-        
-        for(int i=0;i<coins.length;i++){
-            
-          for(int j=1;j<=amount;j++){
-            
-            if(j>=coins[i]){
-               temp[j]= temp[j-coins[i]]+temp[j];
-            }
-            
-        }   
-            
-        }
-        
-        return temp[amount];
-    }
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = 1; i <= amount; i++) {
+                if (i >= coin) {
+                    dp[i] += dp[i - coin];    
+                }
+            }
+        }
+        return dp[amount];
+    }
 }
