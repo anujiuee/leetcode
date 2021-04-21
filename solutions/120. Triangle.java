6f9d0dea15bc31337ac913672776bb7029566c1a
anujiuee <anujkunalarora@gmail.com>
2021-04-21 22:00:40 +0530

    5 ms

---
+++ solutions/120. Triangle.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+     
+        int n = triangle.size();
+        if(n==0) return 0;
+        
+        for (int i=n-2;i>=0;i--){
+            
+            
+            for (int j=0;j<triangle.get(i).size();j++){
+               triangle.get(i).set(j,triangle.get(i).get(j)+Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1)));
+            }
+            
+        }
+        
+        return triangle.get(0).get(0);
+    }
+}
